a953166747ff1f6691c4cc1449363cf11bf012e9
CyZeTLC <tom@cyzetlc.de>
2025-11-14 22:25:49 +0100

    setHealth methode now calls the PlayerDamageEvent

--- core/src/main/java/de/cyzetlc/hsbi/game/entity/Player.java
+++ core/src/main/java/de/cyzetlc/hsbi/game/entity/Player.java
@@ -1,10 +1,22 @@
 package de.cyzetlc.hsbi.game.entity;
 
+import de.cyzetlc.hsbi.game.event.impl.damage.DamageCause;
+import de.cyzetlc.hsbi.game.event.impl.damage.PlayerDamageEvent;
+
 import java.util.Random;
 
 public class Player extends Entity {
     public Player() {
+        super();
         this.setDisplayName("Player" + new Random().nextInt(999));
+    }
+
+    @Override
+    public void setHealth(float health) {
+        if (health < this.getHealth()) {
+            new PlayerDamageEvent(this, this.getHealth()-health, DamageCause.ITEM).call();
+        }
+        super.setHealth(health);
     }
 
     @Override
