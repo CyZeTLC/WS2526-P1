84a0e65649c619ed4953381194c4f05f797699dd
Phantomic813 <Leonardorosarioparrino@gmail.com>
2025-11-15 23:47:08 +0100

    Update Material.java
    Test FloatingPlatform

--- client/src/main/java/de/cyzetlc/hsbi/game/gui/block/Material.java
+++ client/src/main/java/de/cyzetlc/hsbi/game/gui/block/Material.java
@@ -4,7 +4,8 @@ public enum Material {
     AIR(""),
     DIRT("/assets/tileset/dirt_tiles.png"),
     LAVA("/assets/tileset/LAVA_tiles.png"),
-    JUMP_PERK("/assets/tileset/jumpboost.png");
+    JUMP_PERK("/assets/tileset/jumpboost.png"),
+    FLOATING_PLATFORM("/assets/movingplatform/1MovingPlatform32x64.png");
 
     String texturePath;
 
