7d393ff59bf3a7c62f3c60402c9132010233ec0c
Phantomic813 <Leonardorosarioparrino@gmail.com>
2025-11-16 21:40:27 +0100

    Update Music.java

--- client/src/main/java/de/cyzetlc/hsbi/game/audio/Music.java
+++ client/src/main/java/de/cyzetlc/hsbi/game/audio/Music.java
@@ -1,8 +1,8 @@
 ﻿package de.cyzetlc.hsbi.game.audio;
 
 public enum Music {
-    MENU("assets/audio/background_music/Sonic Empire (Short Mix) (128kbit_AAC).m4a"),
-    GAME("assets/audio/background_music/Sonic Empire (Short Mix) (128kbit_AAC).m4a");
+    MENU("/assets/audio/background_music/Sonic Empire (Short Mix) (128kbit_AAC).m4a"),
+    GAME("/assets/audio/background_music/Sonic Empire (Short Mix) (128kbit_AAC).m4a");
 
     private final String path;
 
