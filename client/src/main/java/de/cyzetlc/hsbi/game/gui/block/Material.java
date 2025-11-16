a2b3e6c24f0b2a005c21054162ac86e11721580d
Phantomic813 <Leonardorosarioparrino@gmail.com>
2025-11-16 15:51:51 +0100

    Update Material.java

--- client/src/main/java/de/cyzetlc/hsbi/game/gui/block/Material.java
+++ client/src/main/java/de/cyzetlc/hsbi/game/gui/block/Material.java
@@ -3,7 +3,7 @@ package de.cyzetlc.hsbi.game.gui.block;
 public enum Material {
     AIR(""),
     DIRT("/assets/tileset/dirt_tiles.png"),
-    LAVA("/assets/tileset/LAVA_tiles.png"),
+    LAVA("/assets/lavaset/1Lava32x64.png"),
     JUMP_PERK("/assets/tileset/jumpboost.png"),
     FLOATING_PLATFORM("/assets/movingplatform/1MovingPlatform32x64.png");
 
