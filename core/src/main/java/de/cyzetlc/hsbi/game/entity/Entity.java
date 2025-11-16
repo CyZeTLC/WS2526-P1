fc012c759189961008866254854d4ad65c8896e5
Phantomic813 <Leonardorosarioparrino@gmail.com>
2025-11-16 16:02:59 +0100

    Update Entity.java

--- core/src/main/java/de/cyzetlc/hsbi/game/entity/Entity.java
+++ core/src/main/java/de/cyzetlc/hsbi/game/entity/Entity.java
@@ -14,13 +14,19 @@ public abstract class Entity {
     private String displayName;
 
     private float health;
+    private float maxHealth;
 
     private float width;
     private float height;
 
     public Entity() {
         this.uuid = UUID.randomUUID();
-        this.health = 1.0F;
+        this.maxHealth = 1.0F; // 100% Leben als 1.0
+        this.health = this.maxHealth;
+    }
+
+    public void setHealth(float health) {
+        this.health = Math.max(0F, Math.min(health, this.maxHealth));
     }
 
     public abstract void update();
