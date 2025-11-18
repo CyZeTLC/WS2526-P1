edaf4dcd19ad3be5ed6bccf6afd37b806d31ccc0
Tom <57322153+CyZeTLC@users.noreply.github.com>
2025-11-18 18:58:26 +0100

    Added Background-Animation in MainMenu

--- client/src/main/java/de/cyzetlc/hsbi/game/gui/screens/MainMenuScreen.java
+++ client/src/main/java/de/cyzetlc/hsbi/game/gui/screens/MainMenuScreen.java
@@ -5,9 +5,14 @@ import de.cyzetlc.hsbi.game.audio.SoundManager;
 import de.cyzetlc.hsbi.game.gui.ScreenManager;
 import de.cyzetlc.hsbi.game.gui.GuiScreen;
 import de.cyzetlc.hsbi.game.utils.ui.UIUtils;
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
+import javafx.util.Duration;
 import oshi.SystemInfo;
 import oshi.hardware.CentralProcessor;
 import oshi.hardware.GlobalMemory;
@@ -28,7 +33,42 @@ public class MainMenuScreen implements GuiScreen {
         double width = screenManager.getStage().getWidth();
         double height = screenManager.getStage().getHeight();
 
-        UIUtils.drawImage(root, "/assets/hud/background.png", 0, 0, width, height);
+        /*
+        Background mit Bewegung von links nach rechts (so ähnlich wie in Minecraft halt)
+         */
+        ImageView bg1 = UIUtils.drawImage(root, "/assets/hud/background.png", 0, 0, width, height);
+        ImageView bg2 = UIUtils.drawImage(root, "/assets/hud/background.png", 0, 0, width, height);
+
+        bg1.setFitWidth(width);
+        bg1.setFitHeight(height);
+
+        bg2.setFitWidth(width);
+        bg2.setFitHeight(height);
+        bg2.setTranslateX(width);
+
+        double speed = 1;
+
+        Timeline timeline = new Timeline(
+                new KeyFrame(Duration.millis(16), e -> {
+
+                    // beide verschieben sich nach rechts
+                    bg1.setTranslateX(bg1.getTranslateX() + speed);
+                    bg2.setTranslateX(bg2.getTranslateX() + speed);
+
+                    // Wenn ein Bild rechts komplett raus ist -> an linke Position setzen
+                    if (bg1.getTranslateX() >= width) {
+                        bg1.setTranslateX(bg2.getTranslateX() - width);
+                    }
+
+                    if (bg2.getTranslateX() >= width) {
+                        bg2.setTranslateX(bg1.getTranslateX() - width);
+                    }
+                })
+        );
+
+        timeline.setCycleCount(Animation.INDEFINITE);
+        timeline.play();
+
         UIUtils.drawCenteredText(root, "STEAL THE FILES", 0, height / 2 - 230, false).setId("menu-title");
         UIUtils.drawCenteredButton(root, "Spiel starten", 0, height / 2 - 150, false, "mainmenu-button", () -> screenManager.showScreen(new GameScreen(screenManager)));
         UIUtils.drawCenteredButton(root, "Mehrspieler", 0, height / 2 - 70, false, "mainmenu-button", () -> screenManager.showScreen(new MultiplayerScreen(screenManager)));
