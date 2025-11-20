c8947c7a51725521f46046f754e315cbc4367b7f
Phantomic813 <Leonardorosarioparrino@gmail.com>
2025-11-20 13:45:16 +0100

    JumpBoost+TonV1

--- client/src/main/java/de/cyzetlc/hsbi/game/audio/Sound.java
+++ client/src/main/java/de/cyzetlc/hsbi/game/audio/Sound.java
@@ -4,7 +4,8 @@ public enum Sound {
     CLICK("/assets/audio/menu_click.wav"),
     ENEMY_DIE("/assets/audio/enemy_die.mp3"),
     PLAYER_DIE("/assets/audio/player_die.mp3"),
-    JUMP("/assets/audio/jump.mp3");
+    JUMP("/assets/audio/jump.mp3"),
+    JUMP_BOOST("/assets/audio/SoundEvents/Jump-Boost-Soundwav.wav");
 
     public final String path;
 
