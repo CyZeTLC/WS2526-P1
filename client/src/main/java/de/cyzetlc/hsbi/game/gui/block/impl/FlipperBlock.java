edc7dc868e323cb2a55bb701833888479c524a3d
Phantomic813 <Leonardorosarioparrino@gmail.com>
2025-11-27 16:36:55 +0100

    FlipperHinzugefuegt
    Der ist noch ohne Wav Sound

---
+++ client/src/main/java/de/cyzetlc/hsbi/game/gui/block/impl/FlipperBlock.java
@@ -0,0 +1,28 @@
+package de.cyzetlc.hsbi.game.gui.block.impl;
+
+import de.cyzetlc.hsbi.game.audio.Sound;
+import de.cyzetlc.hsbi.game.audio.SoundManager;
+import de.cyzetlc.hsbi.game.entity.Player;
+import de.cyzetlc.hsbi.game.gui.block.Material;
+import de.cyzetlc.hsbi.game.gui.block.PerkBlock;
+import de.cyzetlc.hsbi.game.world.Location;
+
+public class FlipperBlock extends PerkBlock {
+    private boolean collected = false;
+
+    public FlipperBlock(Location location) {
+        super(location);
+        this.setMaterial(Material.FLIPPER);
+        this.setCollideAble(false);
+    }
+
+    @Override
+    public void onCollide(Player player) {
+        if (this.collected) {
+            return;
+        }
+        this.collected = true;
+        SoundManager.play(Sound.CLICK);
+        this.setActive(false);
+    }
+}
