b87d3cb2c2d75b189068474bc4fd179748c2737c
Phantomic813 <Leonardorosarioparrino@gmail.com>
2025-11-27 16:49:26 +0100

    FlipperBlockFix

--- client/src/main/java/de/cyzetlc/hsbi/game/gui/block/impl/FlipperBlock.java
+++ client/src/main/java/de/cyzetlc/hsbi/game/gui/block/impl/FlipperBlock.java
@@ -1,11 +1,15 @@
 package de.cyzetlc.hsbi.game.gui.block.impl;
 
+import de.cyzetlc.hsbi.game.Game;
 import de.cyzetlc.hsbi.game.audio.Sound;
 import de.cyzetlc.hsbi.game.audio.SoundManager;
 import de.cyzetlc.hsbi.game.entity.Player;
 import de.cyzetlc.hsbi.game.gui.block.Material;
 import de.cyzetlc.hsbi.game.gui.block.PerkBlock;
+import de.cyzetlc.hsbi.game.gui.screens.GameScreen;
+import de.cyzetlc.hsbi.game.utils.ui.ImageAssets;
 import de.cyzetlc.hsbi.game.world.Location;
+import javafx.scene.layout.Pane;
 
 public class FlipperBlock extends PerkBlock {
     private boolean collected = false;
@@ -14,8 +18,8 @@ public class FlipperBlock extends PerkBlock {
         super(location);
         this.setMaterial(Material.FLIPPER);
         this.setCollideAble(false);
-        this.setWidth(96);
-        this.setHeight(48);
+        this.setWidth(0);
+        this.setHeight(0);
     }
 
     @Override
@@ -26,5 +30,34 @@ public class FlipperBlock extends PerkBlock {
         this.collected = true;
         SoundManager.play(Sound.CLICK);
         this.setActive(false);
+    }
+
+    @Override
+    public void draw(Pane pane) {
+        this.pane = pane;
+        this.sprite = ImageAssets.getBlockImage(this.getMaterial());
+        this.sprite.setPreserveRatio(true);
+
+        double targetWidth = 140;
+        this.sprite.setFitWidth(targetWidth);
+        this.sprite.setFitHeight(targetWidth); // height will be reduced by preserveRatio
+
+        double camX = 0;
+        double camY = 0;
+        if (Game.getInstance() != null && Game.getInstance().getScreenManager().getCurrentScreen() instanceof GameScreen) {
+            GameScreen gs = (GameScreen) Game.getInstance().getScreenManager().getCurrentScreen();
+            camX = gs.getCameraX();
+            camY = gs.getCameraY();
+        }
+
+        this.sprite.setX(this.getLocation().getX() - camX);
+        this.sprite.setY(this.getLocation().getY() - camY);
+
+        this.setWidth((float) this.sprite.getBoundsInLocal().getWidth());
+        this.setHeight((float) this.sprite.getBoundsInLocal().getHeight());
+
+        if (!pane.getChildren().contains(sprite)) {
+            pane.getChildren().add(sprite);
+        }
     }
 }
